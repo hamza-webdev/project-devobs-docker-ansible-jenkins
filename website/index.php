8a5678943879efa563ddd165594b24844faecddf
Hamza BEDOUI <hamza.bedoui@ausy.fr>
2022-06-20 15:03:32 +0200

    init project

---
+++ website/index.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <title>My Shop</title>
+</head>
+
+<body>
+    <h1>Welcome to my shop (test project devops</h1>
+    <ul>
+        <?php
+
+        $json = file_get_contents('http://product-service/');
+        $obj = json_decode($json);
+
+        $products = $obj->products;
+
+        foreach ($products as $product) {
+            echo "<li>$product</li>";
+        }
+
+        ?>
+    </ul>
+</body>
+
+</html>
